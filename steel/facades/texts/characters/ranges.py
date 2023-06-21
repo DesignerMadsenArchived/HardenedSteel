86980b8faebad47374e9647886610aa869382bbb
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-22 00:15:05 +0200

    progress

--- steel/facades/texts/characters/ranges.py
+++ steel/facades/texts/characters/ranges.py
@@ -1,18 +1,32 @@
 from steel.facades.texts.characters \
     import get_system_random
 
+ascii_lowercase_begin: int = ord('a')
+ascii_lowercase_end: int = ord('z')
+
+ascii_uppercase_begin: int = ord('A')
+ascii_uppercase_end: int = ord('Z')
+
 
 def random_uppercase_letter() -> str:
+    global                          \
+        ascii_uppercase_begin,      \
+        ascii_uppercase_end
+
     return character_in_range(
-        ord('A'),
-        ord('Z')
+        ascii_uppercase_begin,
+        ascii_uppercase_end
     )
 
 
 def random_lowercase_letter() -> str:
+    global                          \
+        ascii_lowercase_begin,      \
+        ascii_lowercase_end
+
     return character_in_range(
-        ord('a'),
-        ord('z')
+        ascii_lowercase_begin,
+        ascii_lowercase_end
     )
 
 
