c9f7dcbe2436093f87e1b4aa4ab2d5a31ed86cf8
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-22 14:59:16 +0200

    import added
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

--- HardenedSteel/facades/__init__.py
+++ HardenedSteel/facades/__init__.py
@@ -1,16 +1,21 @@
+from HardenedSteel.facades.binaries             \
+    import                                      \
+    generate_random_bit,                        \
+    generate_random_bits,                       \
+    generate_random_bytes
+
 from HardenedSteel.facades.booleans             \
     import generate_random_boolean
 
-
 from HardenedSteel.facades.texts                \
-    import                              \
-    generate_lowercase_label_by_size,   \
-    generate_uppercase_label_by_size,   \
+    import                                      \
+    generate_lowercase_label_by_size,           \
+    generate_uppercase_label_by_size,           \
     generate_label_by_size
 
 
 from HardenedSteel.facades.integers             \
-    import                              \
-    generate_unsigned_integer,          \
+    import                                      \
+    generate_unsigned_integer,                  \
     generate_signed_integer
 
---
+++ HardenedSteel/facades/binaries/__init__.py
@@ -0,0 +1,5 @@
+from HardenedSteel.facades.binaries.random_binary_generation    \
+    import                                                      \
+    generate_random_bit,                                        \
+    generate_random_bits,                                       \
+    generate_random_bytes
