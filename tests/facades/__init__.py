bfa30d7fe1c120c43f57b9bace1fbec6e3a83d64
Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>
2023-06-22 11:30:08 +0200

    unsigned & signed integers
    Signed-off-by: Madsen, Kent <kent.vejrup.madsen@designermadsen.dk>

--- tests/facades/__init__.py
+++ tests/facades/__init__.py
@@ -1,5 +1,15 @@
-from steel.facades                      \
-    import                              \
-    generate_lowercase_label_by_size,   \
-    generate_uppercase_label_by_size,   \
-    generate_label_by_size
+from steel                                  \
+    import                                  \
+    generate_lowercase_label_by_size,       \
+    generate_uppercase_label_by_size,       \
+    generate_label_by_size,                 \
+    generate_unsigned_integer,              \
+    generate_signed_integer
+
+
+from steel.globals                          \
+    import                                  \
+    get_maximum_size_of_unsigned_integer,   \
+    get_zero,                               \
+    get_minimum_size_of_integer,            \
+    get_maximum_size_of_integer
---
+++ tests/facades/test_integer.py
@@ -0,0 +1,33 @@
+from tests.facades                                                                      \
+    import                                                                              \
+    generate_unsigned_integer,                                                          \
+    generate_signed_integer,                                                            \
+    get_zero,                                                                           \
+    get_maximum_size_of_unsigned_integer,                                               \
+    get_minimum_size_of_integer,                                                        \
+    get_maximum_size_of_integer
+
+
+def test() -> None:
+    print()
+    assert True
+
+
+def test_signed() -> None:
+    example: int = generate_signed_integer()
+    print('example-signed: ', example)
+
+    assert                                                                              \
+        (get_minimum_size_of_integer() <= example)                                      \
+        and                                                                             \
+        (example <= get_maximum_size_of_integer())
+
+
+def test_unsigned() -> None:
+    example: int = generate_unsigned_integer()
+    print('example-unsigned: ', example)
+
+    assert                                                                              \
+        (get_zero() <= example)                                                         \
+        and                                                                             \
+        (example <= get_maximum_size_of_unsigned_integer())
